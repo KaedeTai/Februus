bca52bd5418dd62626a79f221a7832ef58305947
Kaede Tai <kaede@richi.com>
2013-09-14 21:55:33 +0800

    新增 Richi VIP

--- html/vip/vip.php
+++ html/vip/vip.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * vip API
+ * Richi VIP API
  *
- * 販賣機vip API
+ * Richi VIP API
  * @package API
  * @author Kaede
  */
@@ -14,7 +14,7 @@ class vip extends Febric {
 	 * 設定資料庫與時區等參數
 	 */
 	function __construct() {
-		mysql_pconnect('rds.richi.com', 'vip', 'vip');
+		mysql_pconnect('vip.richi.com', 'vip', 'vip');
 		mysql_select_db('vip');
 		mysql_query('set names utf8');
 		mysql_query("set time_zone = 'Asia/Taipei'");
@@ -33,8 +33,8 @@ class vip extends Febric {
 
 		//設定SQL
 		$order_sql   = "SELECT o.*, u.user_name, u.phone
-						FROM   `order` o LEFT JOIN user u ON o.user_id=u.user_id
-						WHERE  o.state IN ('Create', 'Confirm') AND shop_id=?";
+				FROM   `order` o LEFT JOIN user u ON o.user_id=u.user_id
+				WHERE  o.state IN ('Create', 'Confirm') AND shop_id=?";
 		$item_sql    = 'SELECT * FROM item WHERE order_id=?';
 		$toggle_sql  = 'SELECT * FROM item_toggle WHERE item_id=?';
 		$option_sql  = 'SELECT * FROM item_option WHERE item_id=?';
@@ -67,22 +67,22 @@ class vip extends Febric {
 		//設定SQL
 		$menu_sql    = 'SELECT menu_id, menu_name FROM menu WHERE merchant_id=? AND enable=1 ORDER BY sort';
 		$product_sql = 'SELECT p.product_id, p.product_name, m.price
-						FROM   product p, menu_product mp, model m
-						WHERE  p.product_id=mp.product_id AND mp.menu_id=? AND p.enable=1 AND p.model_id=m.model_id
-						ORDER BY p.sort';
+				FROM   product p, menu_product mp, model m
+				WHERE  p.product_id=mp.product_id AND mp.menu_id=? AND p.enable=1 AND p.model_id=m.model_id
+				ORDER BY p.sort';
 		$model_sql   = 'SELECT model_id, model_name, price FROM model WHERE product_id=? ORDER BY sort';
 		$toggle_sql  = 'SELECT t.toggle_id, t.toggle_name, pt.plus
-						FROM   product_toggle pt, toggle t
-						WHERE  pt.product_id=? AND pt.toggle_id=t.toggle_id AND t.enable=1
-						ORDER BY t.sort';
+				FROM   product_toggle pt, toggle t
+				WHERE  pt.product_id=? AND pt.toggle_id=t.toggle_id AND t.enable=1
+				ORDER BY t.sort';
 		$select_sql  = 'SELECT s.select_id, s.select_name
-						FROM   product_select ps, `select` s
-						WHERE  ps.product_id=? AND ps.select_id=s.select_id AND s.enable=1
-						ORDER BY s.sort';
+				FROM   product_select ps, `select` s
+				WHERE  ps.product_id=? AND ps.select_id=s.select_id AND s.enable=1
+				ORDER BY s.sort';
 		$option_sql  = 'SELECT o.option_id, o.option_name, po.plus
-						FROM   product_option po, `option` o
-						WHERE  o.select_id=? AND po.product_id=? AND po.option_id=o.option_id
-						ORDER BY o.sort';
+				FROM   product_option po, `option` o
+				WHERE  o.select_id=? AND po.product_id=? AND po.option_id=o.option_id
+				ORDER BY o.sort';
 
 		//讀取資料
 		$menu = queryArray($menu_sql, $merchant_id);
